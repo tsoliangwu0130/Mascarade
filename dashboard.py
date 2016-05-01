8df97cde445b8e4ae2c4b49883d5a6863a04db65
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-05-01 16:42:01 -0700

    Updated players number input

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ import random
 # All roles available
 allRolesList = ["spy", "bishop", "fool", "inquisitor", "judge", "peasant", "queen", "king", "witch", "cheat", "widow", "thief", "beggar"]
 
+# Set player numbers
 while True:
 	try:
 		playersNumber = int(raw_input("Number of players (4 - %d)? " %len(allRolesList)))
