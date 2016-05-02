e57c345b6bda0d0bf1ec2c5934bca3259d8a4b0b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-05-02 00:00:41 -0700

    Add coin attribute to role class

--- role/role.py
+++ role/role.py
@@ -2,9 +2,13 @@ class Role:
 	def __init__(self, roleName, roleAbility):
 		self.roleName    = roleName
 		self.roleAbility = roleAbility
+		self.coin        = coin
 	
 	def getRoleName(self):
 		return self.roleName
 
 	def getAbility(self):
 		return self.roleAbility
+
+	def getCoin(self):
+		return self.coin
