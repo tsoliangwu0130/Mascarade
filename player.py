7a49a01c9228d8149413b6c3cf80edb2fadc850e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-05-14 00:02:40 -0700

    Update swap, glance
    create two funciton to send / receive request

--- player.py
+++ player.py
@@ -1,13 +1,20 @@
 import random
 
-# initial variables
+# initial properties
 initialCoin        = 6
 initialStatus      = "private"
 availableRolesList = ["spy", "bishop", "fool", "inquisitor", "judge", 
                       "peasant", "peasant", "queen", "king", "witch", 
                       "cheat", "widow", "thief", "beggar"]
 
+def sendRequest(request):
+	print "send", request
+
+def receiveRequest(request):
+	print "receive", request
+
 class Player():	
+	# each player must to be created by giving an actual role
 	def __init__(self, actualRole):
 		self.coin          = initialCoin # current holding coins
 		self.suspectedRole = random.choice(availableRolesList) # suspected role identity
@@ -15,15 +22,16 @@ class Player():
 		self.status        = initialStatus # player status (public / private)
 
 	def swap(self):
-		pass
+		sendRequest(1)
+		receiveRequest(2)
+
 	def glance(self):
-		pass
+		print "*** Glance: You are the " + self.actualRole + " ! ***"
+		self.suspectedRole = self.actualRole
+
 	def declare(self):
 		pass
 
 # testing
-player = Player(random.choice(availableRolesList))
-print "play coin:", player.coin
-print "play suspectedRole:", player.suspectedRole
-print "play actualRole:", player.actualRole
-print "play status:", player.status
+player = Player("king")
+player.swap()
